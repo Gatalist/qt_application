ba64d4331d02d9dc425f6971a6c18a59a8e24d12
Gatalist <kostencko.alexander2012@gmail.com>
2026-05-05 22:15:14 +0300

    fix duplicate code

--- pages/image/crop/widget.py
+++ pages/image/crop/widget.py
@@ -1,37 +1,8 @@
-from PyQt5.QtWidgets import QTableWidget, QApplication
-from PyQt5.QtGui import QKeySequence
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from components.copyable_table import CopyableTableWidget
 from components.image import ImageManager
 from .UI_window import Ui_Form
-
-
-class CopyableTableWidget(QTableWidget):
-    def keyPressEvent(self, event):
-        if event.matches(QKeySequence.Copy):
-            self.copy_selection()
-        else:
-            super().keyPressEvent(event)
-
-    def copy_selection(self):
-        selection = self.selectedIndexes()
-
-        if not selection:
-            return
-
-        selection.sort(key=lambda x: (x.row(), x.column()))
-        rows = {}
-        for index in selection:
-            item = self.item(index.row(), index.column())
-            if item:
-                rows.setdefault(index.row(), {})[index.column()] = item.text()
-
-        copied_text = ''
-        for row in sorted(rows):
-            line = '\t'.join(rows[row].get(col, '') for col in sorted(rows[row]))
-            copied_text += line + '\n'
-
-        QApplication.clipboard().setText(copied_text.strip())
 
 
 class CropWorker(QThread):
